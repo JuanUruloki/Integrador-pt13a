40985a71ea6163145511725618806fbe812b0829
JuanUruloki <juampy_1@live.com>
2023-07-11 00:16:29 -0300

    files update

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -1,20 +1,18 @@
-
 let myFavorites = [];
 
 const postFav = (req, res) => {
-    myFavorites.push(req.body)
-    return res.json(myFavorites)
-}
+  myFavorites.push(req.body);
+  return res.json(myFavorites);
+};
 
 const deleteFav = (req, res) => {
-    const {id} = req.params
-    const filtered = myFavorites.filter((fav)=> fav.id !== Number(id) )
-    myFavorites = filtered
-    return res.json(myFavorites)
-
-}
+  const { id } = req.params;
+  const filtered = myFavorites.filter((fav) => fav.id !== Number(id));
+  myFavorites = filtered;
+  return res.status(200).json(myFavorites);
+};
 
 module.exports = {
-    postFav,
-    deleteFav
-}
+  postFav,
+  deleteFav,
+};
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./routes/index");
 const PORT = 3001;
+const { conn } = require("./DB_connection");
 
 // ? --------------- ANTES DE EXPRESS ---------------
 /* const http = require("http");
@@ -9,41 +10,44 @@ const getCharById = require("./controllers/getCharById");
 
 http
   .createServer((req, res) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-  
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    
     if (req.url.includes("/rickandmorty/character")) {
-  const id = Number(req.url(split("/").at(-1)))
-  const id = req.url.split("/").at(-1);
+      const id = Number(req.url(split("/").at(-1)))
+      const id = req.url.split("/").at(-1);
       const charId = data.find((char) => {
         return char.id === +id;
-      getCharById(res, id);
-    }
+        getCharById(res, id);
+      }
   })
       res
         .writeHead(200, { "Content-type": "application/json" })
         .end(JSON.stringify(charId));
-    }
-  })
-   .listen(3001);*/
-
-// *************** DESPUES DE EXPRESS ***************
-
-const server = express();
-
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
-server.use(express.json());
-server.use("/rickandmorty", router);
-
-server.listen(PORT, () => {
-  console.log(`Server raised in port:${PORT}`);
-});
+      }
+    })
+    .listen(3001);*/
+    
+    // *************** DESPUES DE EXPRESS ***************
+    
+    const server = express();
+    
+    server.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+        next();
+      });
+      
+      server.use(express.json());
+      server.use("/rickandmorty", router);
+      
+      server.listen(PORT, () => {
+        console.log(`Server raised in port:${PORT}`);
+      });
+      
+      
+      conn.sync({ force: true });
